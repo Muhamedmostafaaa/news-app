c6153e16b7ca1987671695a12502f3a0e2c5ece8
dell <muhamed_mostafaa12@hotmail.com>
2022-05-26 22:08:03 +0200

    init application

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(myapp());
+}
+class myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+
+}
